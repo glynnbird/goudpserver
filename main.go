c4f59ba21f48426b689e4dfd6d306086b619e4a2
Glynn Bird <glynn.bird@gmail.com>
2025-12-15 16:41:37 +0000

    allow port override

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
-const port int = 8080
+const defaultPort string = "8080"
 
 func main() {
+
+	// look for override of default port using environment variable
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = defaultPort
+	}
+
+	// convert to string to integer
+	log.Println("Listening on port", portStr)
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		fmt.Println("Cannot parse PORT environment variable")
+		os.Exit(1)
+	}
+
+	// run the server
 	server := NewServer(port)
-	err := server.Run()
+	err = server.Run()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
